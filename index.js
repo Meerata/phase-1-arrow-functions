d387c2e74ee2ba55a86059e7fe935617eed46da2
Meerata <merata87@gmail.com>
2024-03-25 11:02:47 +0300

    completed

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function expression for divide
+const divide = function (x, y) {
+  return x / y;
+};
+
+// Arrow function for square
+const square = (x) => x * x;
+
+// Arrow function for add
+const add = (x, y) => x + y;
+
+// Export the functions
+module.exports = {
+  divide,
+  square,
+  add,
+};
